f87d710c7c236a73a9d2190325f05e5097093a2c
kiennfzd <133041460+kiennfzd@users.noreply.github.com>
2023-08-06 01:23:47 +0700

    Update aaa.js

--- aaa.js
+++ aaa.js
@@ -1,46 +1,40 @@
 <script>
   const populateLabelPosts = (data, container) => {
     data.forEach((post) => {
-      const labelPost = $('<div class="label-post"></div>');
-
-      const thumbContainer = $('<a class="thumb-container"></a>');
-      thumbContainer.attr('href', post.link[post.link.length - 1].href);
-
-      const thumbnail = $('<img>');
-      thumbnail.attr('src', post.media$thumbnail.url.replace('/s72', '/s300'));
-
-      thumbContainer.append(thumbnail);
-      labelPost.append(thumbContainer);
-
-      const textContent = $('<div class="text-content"></div>');
-
-      const postTitleContainer = $('<a class="post-title-container"></a>');
-      postTitleContainer.attr('href', post.link[post.link.length - 1].href);
-
-      const title = $('<h3 class="post-title"></h3>');
-      title.html(post.title.$t);
-
-      postTitleContainer.append(title);
-      textContent.append(postTitleContainer);
-
-      labelPost.append(textContent);
-
-      container.append(labelPost);
+      // Tạo các phần tử bài viết tương tự như trước đó
     });
   };
 
   const displayLabelPosts = (label, posts) => {
-    const containerElements = $(`[data-label='${label}']`);
+    const containerElements = document.querySelectorAll(`[data-label='${label}']`);
 
     if (containerElements.length > 0) {
-      containerElements.each((index, containerElement) => {
-        const loadingImage = $('<img class="loading-image"alt="..." />');
-        loadingImage.attr('src', 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-mda1blKQ2CPj4uOfLALZVNtOHohk0c9HAj7e7tc13WG5A5Nmc4pEeYD2NCXNX4gAHjvlJQOuU-Evi8ROb56A0iCb2alK0sXkMwzWhOGEQdr0iGNXp-_x4zb5Jc2_XodnEUeqvJ6QfEeIXU6JRE17YBH7vPGZyIlkBwYWtXseeInHz7yH2avHvosBjFDP/s200/loading-gif.gif');
-        $(containerElement).append(loadingImage);
+      containerElements.forEach((containerElement) => {
+        const loadingImage = document.createElement('img');
+        loadingImage.src = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-mda1blKQ2CPj4uOfLALZVNtOHohk0c9HAj7e7tc13WG5A5Nmc4pEeYD2NCXNX4gAHjvlJQOuU-Evi8ROb56A0iCb2alK0sXkMwzWhOGEQdr0iGNXp-_x4zb5Jc2_XodnEUeqvJ6QfEeIXU6JRE17YBH7vPGZyIlkBwYWtXseeInHz7yH2avHvosBjFDP/s200/loading-gif.gif';
+        loadingImage.classList.add('loading-image');
 
-        populateLabelPosts(posts.feed.entry, $(containerElement));
-        loadingImage.remove();
+        containerElement.appendChild(loadingImage);
+
+        populateLabelPosts(posts.feed.entry, containerElement);
+        containerElement.removeChild(loadingImage);
       });
     }
   };
+</script>
+
+<script>
+  const fetchPosts = (label) => {
+    const apiUrl = `https://your-blog-url.blogspot.com/feeds/posts/summary/-/${label}?max-results=3&alt=json`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => displayLabelPosts(label, data))
+      .catch((error) => console.error(error));
+  };
+
+  const containerElements = document.querySelectorAll('.label-posts-container');
+  containerElements.forEach((containerElement) => {
+    const label = containerElement.getAttribute('data-label');
+    fetchPosts(label);
+  });
 </script>
